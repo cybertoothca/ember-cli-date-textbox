3f0cd305a0878079b9c4cfe9ba84d59e8c2c5fe2
Dan Nelson <dnelson@cybertooth.ca>
2017-02-25 19:59:27 -0700

    #1 Fixing the import of moment & date.js.

--- blueprints/ember-cli-date-textbox/index.js
+++ blueprints/ember-cli-date-textbox/index.js
@@ -4,17 +4,11 @@ module.exports = {
   name: 'ember-cli-date-textbox',
   normalizeEntityName: function () {
   },
-  included: function (app) {
-    this._super.included(app);
-    app.import(`${app.bowerDirectory}/date.js/build/date-en-US.js`);
-    app.import(`${app.bowerDirectory}/moment/moment.js`);
-    app.import(`${app.bowerDirectory}/moment-timezone/builds/moment-timezone-with-data.js`);
-  // },
-  // afterInstall: function (/*options*/) {
-  //   return this.addBowerPackagesToProject([
-  //     {name: 'date.js'},
-  //     {name: 'moment'},
-  //     {name: 'moment-timezone'}
-  //   ]);
+  afterInstall: function (/*options*/) {
+    return this.addBowerPackagesToProject([
+      {name: 'date.js'},
+      {name: 'moment'},
+      {name: 'moment-timezone'}
+    ]);
   }
 };
