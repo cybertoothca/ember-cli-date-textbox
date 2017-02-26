83f5ee24e2c04d368212a533dba81e631b6c1d40
Dan Nelson <dnelson@cybertooth.ca>
2017-02-25 19:09:49 -0700

    #1 Fixes from code review.

--- blueprints/ember-cli-date-textbox/index.js
+++ blueprints/ember-cli-date-textbox/index.js
@@ -4,14 +4,15 @@ module.exports = {
   description: 'A textbox that will guess the date you want and assign it to your model or query-params.',
   included: function (app) {
     this._super.included(app);
-    app.import(app.bowerDirectory + '/showdown/dist/showdown.js');
-    app.import(app.bowerDirectory + '/underscore.string/dist/underscore.string.js');
-  },
+    app.import(`${app.bowerDirectory}/date.js/build/date-en-US.js`);
+    app.import(`${app.bowerDirectory}/moment/moment.js`);
+    app.import(`${app.bowerDirectory}/moment-timezone/builds/moment-timezone-with-data.js`);
+  // },
   // afterInstall: function (/*options*/) {
   //   return this.addBowerPackagesToProject([
   //     {name: 'date.js'},
   //     {name: 'moment'},
   //     {name: 'moment-timezone'}
   //   ]);
-  // }
+  }
 };
