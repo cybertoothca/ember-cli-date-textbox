2e3900b9467440589585d9f850856692e2a5b32b
Dan Nelson <dnelson@cybertooth.ca>
2017-02-25 19:57:20 -0700

    #1 Playing around with the blueprint.

--- blueprints/ember-cli-date-textbox/index.js
+++ blueprints/ember-cli-date-textbox/index.js
@@ -1,7 +1,9 @@
 /*jshint node:true*/
 module.exports = {
-  name: 'ember-cli-date-textbox',
   description: 'A textbox that will guess the date you want and assign it to your model or query-params.',
+  name: 'ember-cli-date-textbox',
+  normalizeEntityName: function () {
+  },
   included: function (app) {
     this._super.included(app);
     app.import(`${app.bowerDirectory}/date.js/build/date-en-US.js`);
