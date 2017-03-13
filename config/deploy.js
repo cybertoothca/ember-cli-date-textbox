ac3ea65faa7015896a12d68f1524f928fe9203b7
Dan Nelson <dnelson@cybertooth.ca>
2017-03-12 23:54:37 -0600

    deploy.js needed to be tweaked to always deploy to production.

--- config/deploy.js
+++ config/deploy.js
@@ -10,19 +10,17 @@ module.exports = function (deployTarget) {
       region: 'us-west-2'
     }
   };
+  ENV.build.environment = 'production';
 
   if (deployTarget === 'development') {
-    ENV.build.environment = 'development';
     // configure other plugins for development deploy target here
   }
 
   if (deployTarget === 'staging') {
-    ENV.build.environment = 'production';
     // configure other plugins for staging deploy target here
   }
 
   if (deployTarget === 'production') {
-    ENV.build.environment = 'production';
     // configure other plugins for production deploy target here
   }
 
