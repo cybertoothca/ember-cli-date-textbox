36248462b5d692f6b4144562cbb39f5f385e69d9
Dan Nelson <dnelson@cybertooth.ca>
2017-03-19 15:53:19 -0600

    Adjust the deploy.js to always deploy to production (minifies).

--- config/deploy.js
+++ config/deploy.js
@@ -2,7 +2,9 @@
 
 module.exports = function (deployTarget) {
   var ENV = {
-    build: {},
+    build: {
+      environment: 'production'
+    },
     s3: {
       bucket: 'ember-cli-date-textbox.cybertooth.io',
       filePattern: '**/*.{js,css,png,gif,ico,jpg,map,xml,txt,svg,swf,eot,ttf,woff,woff2,otf,html}',
@@ -10,19 +12,6 @@ module.exports = function (deployTarget) {
       region: 'us-west-2'
     }
   };
-  ENV.build.environment = 'production';
-
-  if (deployTarget === 'development') {
-    // configure other plugins for development deploy target here
-  }
-
-  if (deployTarget === 'staging') {
-    // configure other plugins for staging deploy target here
-  }
-
-  if (deployTarget === 'production') {
-    // configure other plugins for production deploy target here
-  }
 
   // Note: if you need to build some configuration asynchronously, you can return
   // a promise that resolves with the ENV object instead of returning the
