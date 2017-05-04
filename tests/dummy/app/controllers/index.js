339027b033e09fe8a56ed32838af82c920ecaff3
Dan Nelson <dnelson@cybertooth.ca>
2017-05-04 13:52:16 -0600

    Polish the demo turd for exchangejs demo.

--- tests/dummy/app/controllers/index.js
+++ tests/dummy/app/controllers/index.js
@@ -3,16 +3,24 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   dateAsParam: new Date().toISOString(),
   dateWithTimezone: null,
-  iso8601WithTimezone: '',
-  naftaSigned: new Date(1987, 9, 3),
-  now: new Date(),
-  submittedIso8601: '',
-  wasEmptyUntilNow: '',
   dateWithTimezoneISOString: Ember.computed('dateWithTimezone', function () {
     if (Ember.isPresent(this.get('dateWithTimezone'))) {
       return this.get('dateWithTimezone').toISOString();
     } else {
       return 'Choose A Date...';
     }
-  })
+  }),
+  demoDate: null,
+  demoDateISOString: Ember.computed('demoDate', function () {
+    if (Ember.isPresent(this.get('demoDate'))) {
+      return this.get('demoDate').toISOString();
+    } else {
+      return 'Choose A Date...';
+    }
+  }),
+  iso8601WithTimezone: '',
+  naftaSigned: new Date(1987, 9, 3),
+  now: new Date(),
+  submittedIso8601: '',
+  wasEmptyUntilNow: ''
 });
